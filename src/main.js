2cffd05f19029f01939352b7c5cc33e6448bc7b5
maydayola <mayday.20162016@gmail.com>
2017-06-19 12:59:35 +0800

    remove vue-resource and add axios

--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ import Vue from 'vue'
 import vueResource from 'vue-resource'
 import App from './App'
 import router from './router'
+import axios from 'axios'
+Vue.prototype.$http = axios
+
+
+
+
 
 Vue.use(vueResource)
 Vue.config.productionTip = false
